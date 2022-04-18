cb0104fca948fd5ff445e3cd2737fb55df447898
kh-434 <khaled.alhamad434@gmail.com>
2022-04-18 22:30:42 +0300

    Condition

---
+++ 01-Conditions.py
@@ -0,0 +1,27 @@
+'''
+x=11
+if x < 10 :
+    print ('Khalid')
+    if x > 4 :
+        print ('Hi')
+
+else:
+    print('Yes')
+    '''
+'''
+username= 'khalid'
+password= '12345'
+if username=='khalid' or password=='123456':
+    print ('GOOD')
+else:
+    print('BAD')
+ '''
+'''
+x=10
+if x>50:
+    print('KH')
+    if x==0:
+        print('95')
+else:
+    print('NOO')
+'''
